d4d2cf15b1e426c14a2ccb37a62d198d8dde4d53
Leo Ismatov <leo.ismatov@sqlservice.se>
2026-08-18 16:24:28 +0200

    organize local SQL DBA collection

--- MyScripts/Agent_Jobs/check_job_steps.sql
+++
@@ -1,5 +0,0 @@
- select j.name, step_id, step_name, command
- from msdb.dbo.sysjobsteps js
- inner join
- msdb.dbo.sysjobs j
- on js.job_id=j.job_id
--- MyScripts/BrentOzar/Brent_Ozar_sps.sql
+++
@@ -1,9 +0,0 @@
-exec [dbo].[sp_Blitz]
-exec [dbo].[sp_BlitzAnalysis] @help=1
-exec [dbo].[sp_BlitzCache]
-exec [dbo].[sp_BlitzFirst]
-exec [dbo].[sp_BlitzIndex]
-exec [dbo].[sp_BlitzInMemoryOLTP]
-exec [dbo].[sp_BlitzLock]
-exec [dbo].[sp_BlitzQueryStore]
-exec [dbo].[sp_BlitzWho]
